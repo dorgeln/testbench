a215effc5efb0e5298a2ca84b9e60864eba3f91b
Andreas Trawöger <atrawog@datamarble.org>
2021-01-21 16:24:38 +0100

    Ansible Vagrant integration

---
+++ ansible-vagrant/scripts/vagrant/apt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get upgrade -y
+sudo apt-get autoclean -y
+sudo apt-get clean -y
+sudo reboot
---
+++ ansible-vagrant/scripts/vagrant/dnf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo dnf update -y
+sudo dnf clean all -y
+sudo reboot
