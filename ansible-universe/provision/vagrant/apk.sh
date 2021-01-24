5f3294321322244a4c82491f51946e1f55cfc8f6
Andreas Trawöger <atrawog@datamarble.org>
2021-01-24 19:49:48 +0100

    New Vagrant configuration

---
+++ ansible-universe/provision/vagrant/apk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apk add python3
+apk upgrade --available
+apk cache clean
---
+++ ansible-universe/provision/vagrant/pacman.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo pacman --noconfirm -Scc
+cat > /etc/pacman.d/mirrorlist <<'EOF'
+Server = https://cloudflaremirrors.com/archlinux/$repo/os/$arch
+EOF
+
+sudo pacman --noconfirm -Syu
+sudo pacman -S --noconfirm python3
+sudo pacman --noconfirm -Scc
+sudo reboot
